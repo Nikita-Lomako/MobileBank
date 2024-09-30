ba14fedf4ada970d60fb5c0f46b410b93f5532d1
Nikita Lomako <nikitich-2006@mail.ru>
2024-10-01 00:51:58 +0300

    реализована форма Validation

--- MobileBank/Forms/Validation.Designer.cs
+++ MobileBank/Forms/Validation.Designer.cs
@@ -28,12 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.PINLabel = new System.Windows.Forms.Label();
+            this.numericUpDownPin = new System.Windows.Forms.NumericUpDown();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.OKButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPin)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(198)))), ((int)(((byte)(65)))));
+            this.CloseButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.CloseButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.CloseButton.ForeColor = System.Drawing.Color.White;
+            this.CloseButton.Location = new System.Drawing.Point(251, -1);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(27, 27);
+            this.CloseButton.TabIndex = 86;
+            this.CloseButton.Text = "x";
+            this.CloseButton.UseVisualStyleBackColor = false;
+            this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
+            // 
+            // PINLabel
+            // 
+            this.PINLabel.AutoSize = true;
+            this.PINLabel.ForeColor = System.Drawing.Color.White;
+            this.PINLabel.Location = new System.Drawing.Point(15, 32);
+            this.PINLabel.Name = "PINLabel";
+            this.PINLabel.Size = new System.Drawing.Size(116, 16);
+            this.PINLabel.TabIndex = 116;
+            this.PINLabel.Text = "Введите ваш PIN";
+            // 
+            // numericUpDownPin
+            // 
+            this.numericUpDownPin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+            this.numericUpDownPin.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numericUpDownPin.ForeColor = System.Drawing.Color.White;
+            this.numericUpDownPin.Location = new System.Drawing.Point(15, 51);
+            this.numericUpDownPin.Name = "numericUpDownPin";
+            this.numericUpDownPin.Size = new System.Drawing.Size(235, 34);
+            this.numericUpDownPin.TabIndex = 117;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(198)))), ((int)(((byte)(65)))));
+            this.panel3.Font = new System.Drawing.Font("Microsoft Sans Serif", 1.1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.panel3.Location = new System.Drawing.Point(18, 91);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(233, 4);
+            this.panel3.TabIndex = 118;
+            // 
+            // OKButton
+            // 
+            this.OKButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(198)))), ((int)(((byte)(65)))));
+            this.OKButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.OKButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(198)))), ((int)(((byte)(65)))));
+            this.OKButton.FlatAppearance.BorderSize = 0;
+            this.OKButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.OKButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.OKButton.ForeColor = System.Drawing.Color.White;
+            this.OKButton.Location = new System.Drawing.Point(18, 101);
+            this.OKButton.Name = "OKButton";
+            this.OKButton.Size = new System.Drawing.Size(232, 40);
+            this.OKButton.TabIndex = 118;
+            this.OKButton.Text = "ОК";
+            this.OKButton.UseVisualStyleBackColor = false;
+            this.OKButton.Click += new System.EventHandler(this.OKButton_Click);
+            // 
+            // Validation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Validation";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+            this.ClientSize = new System.Drawing.Size(280, 160);
+            this.Controls.Add(this.OKButton);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.numericUpDownPin);
+            this.Controls.Add(this.PINLabel);
+            this.Controls.Add(this.CloseButton);
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Validation";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPin)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button CloseButton;
+        private System.Windows.Forms.Label PINLabel;
+        private System.Windows.Forms.NumericUpDown numericUpDownPin;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button OKButton;
     }
 }
--- MobileBank/Forms/Validation.cs
+++ MobileBank/Forms/Validation.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using MobileBank.Classes;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,50 @@ namespace MobileBank.Forms
 {
     public partial class Validation : Form
     {
+        DataBaseConnection database = new DataBaseConnection();
+        
         public Validation()
         {
             InitializeComponent();
+        }
+
+        private void OKButton_Click(object sender, EventArgs e)
+        {
+            int attemps = 3;
+            int cardPin = Convert.ToInt32(numericUpDownPin.Value);
+            int pin = 0;
+
+            var queryCheckPin = $"select bank_card_pin from bank_card where bank_card_number ='{DataStorage.bankCard}'";
+            SqlCommand command = new SqlCommand(queryCheckPin, database.GetConnection());
+            database.OpenConnection();
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read()) 
+                pin = Convert.ToInt32(reader[0]);            
+            reader.Close();
+
+            if (cardPin == pin)
+            {
+                MessageBox.Show("Операция подтверждена", "Ок", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Close();
+                DataStorage.attempts = attemps;
+            }
+            else
+            {
+                MessageBox.Show("Ошибка. Неверный PIN", "Denied", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                if (attemps > 0)
+                    attemps--;
+                else
+                {
+                    DataStorage.attempts = attemps;
+                    MessageBox.Show("У вас закончились попытки", "Отмена", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    Close();
+                }
+            }
+        }
+
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            Close();
         }
     }
 }
